6eb249a6031cabf42d54ef541f2e9fae88ca1a12
ncorbitt <ncorbitt01@gmail.com>
2020-03-17 11:37:03 -0700

    updated header

---
+++ build/precache-manifest.b87e986129905bd5fed7b99af4a0bce1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5eb4a3ac39e63c34e9d65f83f9fb4e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71dd199372549d36d041",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "728e04b7230688af6bfd",
+    "url": "/static/css/main.bb4f445b.chunk.css"
+  },
+  {
+    "revision": "71dd199372549d36d041",
+    "url": "/static/js/2.248ee3ca.chunk.js"
+  },
+  {
+    "revision": "91bcc555fc9d257ab7cac84356530039",
+    "url": "/static/js/2.248ee3ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728e04b7230688af6bfd",
+    "url": "/static/js/main.fcb4db44.chunk.js"
+  },
+  {
+    "revision": "58a925c0ccd3a8629c8d",
+    "url": "/static/js/runtime-main.1b20ac46.js"
+  },
+  {
+    "revision": "d6a8d38b2866310d4da5d934e548a715",
+    "url": "/static/media/hanson-lu-jYkvecfWgx0-unsplash.d6a8d38b.jpg"
+  },
+  {
+    "revision": "037fbacf1d84609d28999462b6a10eb4",
+    "url": "/static/media/juniperphoton-hmbNJeIzHvY-unsplash.037fbacf.jpg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import {
 	ListGroup,
 	ListGroupItem,
 	FormControl,
-	Breadcrumb
+	Breadcrumb,
+	Navbar
 } from 'react-bootstrap';
 import { Switch, Route, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
@@ -20,6 +21,33 @@ const UserSearchIcon = styled(UserSearch)`
 	width: 50px;
 `;
 
+const Note = styled('section')`
+	color: white;
+	font-size: 1.5em;
+	margin-top: 200px !important;
+	margin-bottom: 30px;
+	
+	& i{
+		color: white;
+		text-shadow: 1px 1px 1px green
+	}
+`
+
+
+function Header() {
+	return (
+		<Navbar id='removeOpacityOnHover' className='shadow' fixed='top' style={{ backgroundColor:'#1f2f40', height:70, opacity:'.7'}}>
+				<Navbar.Brand href='/' className='justify-content-center'>
+					<h6 id='headerTitle' className='mt-2' style={{ fontSize: '2em', textShadow: '1px 1px 7px black' }}>
+						{/* <UserSearch size='48' className='mt-n2 text-white mr-2' /> */}
+						<span className='text-white'>R</span><span style={{ color: '#6bd4c1' }}>eact-</span>
+						<span className='text-white'>S</span><span style={{ color: '#6bd4c1' }}>earch-</span>
+						<span className='text-white'>F</span><span style={{ color: '#6bd4c1' }}>ilter</span>
+					</h6>
+				</Navbar.Brand>
+		</Navbar>
+	)
+}
 
 function SearchBar(props) {
 	const history = useHistory();
@@ -50,44 +78,88 @@ function SearchBar(props) {
 	return (
 		<div>
 			<Container>
-				<Row className='' style={{ marginTop: 'calc(25vh)' }}>
-					<Col className='align-self-center'>
+
+				<Row>
+					<Col>
+					<section id='bg-pic-credit' className='position-fixed border border-white text-white rounded p-3' style={{ top:200, marginLeft:'60%' }}>
+                        <h6 className='mb-2'>Background Picture by</h6>
+                        <a
+                            style={{
+                                backgroundColor: 'black',
+                                color: 'white',
+                                textDecoration: 'none',
+                                padding: '4px 6px',
+                                fontFamily: '-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif',
+                                fontSize: 12, fontWeight: 'bold', lineHeight: '1.2', display: 'inline-block', borderRadius: 3
+                            }}
+                            href="https://unsplash.com/@hansonluu?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Hanson Lu">
+                            <span style={{ display: 'inline-block', padding: '2px 3px' }}>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    style={{ height: 12, width: 'auto', position: 'relative', verticalAlign: 'middle', top: '-2px', fill: 'white' }}
+                                    viewBox="0 0 32 32">
+                                    <title>unsplash-logo</title>
+                                    <path
+                                        d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z">
+                                    </path>
+                                </svg>
+                            </span>
+                            <span style={{ display: 'inline-block', padding: '2px 3px' }}> Hanson Lu </span>
+                        </a>
+                    </section>
+					</Col>
+				</Row>
+				<Row className='' style={{}}>
+					<Col xs={12} sm={12} className='align-self-center'>
+
+						<Note id='Note' className='mt-5 w-75 mx-auto' style={{}}>
+							<p>
+								react-search-filter, is just a basic filter to fiter data, from a input component.
+							</p>
+
+							<p>
+								The example below loads 100 contacts from json data and will be filtered
+								when typing a name. <br/>
+								When you click on contact name, you will be routed
+								to the contact's information.
+							</p>
+
+							<i>
+								React-search-filter, utilizes react-bootstrap, for the look
+								and it's responsiveness.
+							</i>
+						</Note>
+					</Col>
+				</Row>
+
+				<Row>
+					<Col id='search-container' className=''>
 						<h3
-							className='mb-5 pb-3 mt-5'
+							id='title'
+							className='mb-5 pb-3 mt-5 mx-auto'
 							style={{
-								fontSize: '3em',
+								fontSize: '2.5em',
 								borderBottom: '3px solid white',
 								textShadow: '1px 1px 7px black'
 							}}>
-							<span className='text-white'>C</span>
-							<span style={{ color: '#6bd4c1' }}>ontacts:</span>
+							{/* Contacts: */}
+							<span className='text-white'>C</span><span style={{ color: '#6bd4c1' }}>ontacts:</span>
 
-							<i className='float-right' style={{ color: "#6bd4c1" }}>
+							{/* Matches */}
+							<i id='' className='float-right' style={{ color: "#6bd4c1" }}>
 								<span className='text-white'>{matchCount}</span> Matches
 							</i>
 						</h3>
 
-						<h6 className='text-white mb-2'
-							onMouseEnter={e => {
-								e.target.style.backgroundColor = '#142438';
-								e.target.style.padding = '7px';
-								e.target.style.border = '3px solid #6bd4c1';
-							}}
-							onMouseLeave={e => {
-								e.target.style.backgroundColor = '';
-								e.target.style.padding = '0px';
-								e.target.style.border = '';
-							}}>
+						<h4 className='text-white mb-2'>
 
 							Will search firstname and lastname
-						</h6>
+
+						</h4>
 
 						<div
 							className='d-flex rounded py-3'
-							style={{
-								backgroundColor: '#142438',
-								border: '3px solid #6bd4c1'
-							}}>
+							style={{backgroundColor: '#142438',}}>
 							<UserSearchIcon size='36' className='mt-2' />
 							<FormControl
 								className='shadow-lg'
@@ -96,15 +168,12 @@ function SearchBar(props) {
 								type='text'
 								id='searchBox'
 								placeholder='Search for contact . . . '
-								onChange={e => setSearchText(e.target.value)}
+								onChange={e => setSearchText(e.target.value.toLowerCase())}
 							/>
 						</div>
 
 						<ListGroup
-							style={{
-								display: !searchText ? 'none' : '',
-								fontSize: '1.5em'
-							}}>
+							style={{display: !searchText ? 'none' : '',fontSize: '1.5em'}}>
 							{!searchText
 								? ''
 								: contacts.map(contact => {
@@ -132,7 +201,8 @@ function SearchBar(props) {
 												className='list-group-item shadow-lg'
 												style={{
 													backgroundColor: '#142438',
-													cursor: 'pointer'
+													cursor: 'pointer',
+													opacity:'.7'
 												}}
 												onClick={() =>
 													history.push(`/contact/${name}`, [
@@ -158,10 +228,11 @@ function SearchBar(props) {
 											</ListGroupItem>
 										);
 									} else {
-										return null
+										return ''
 									}
 								})}
 						</ListGroup>
+				
 					</Col>
 				</Row>
 			</Container>
@@ -184,22 +255,21 @@ function Contactpage(props) {
 	return (
 		<div>
 			<Container>
-				<Row id='contact-container'>
+				<Row id='contact-container' style={{height: 700}}>
 					<Col
-						className='text-white text-cente align-self-center rounded shadow-lg'
-						style={{
-							backgroundColor: '#142438',
-							border: '3px solid #6bd4c1'
-						}}>
-						<div className='p-5'>
+						id='contact'
+						className='text-white align-self-end rounded shadow-lg'
+						style={{backgroundColor: '#142438',opacity: '.7',}}>
+						<div id='contactCard' className='p-5'>
 							<Breadcrumb
-								className='breadcrumb'
-								style={{ backgroundColor: '#142438' }}>
+								className='breadcrumb ml-n3'
+								style={{ backgroundColor: '#142438', fontSize:'1.5em' }}>
 								<Breadcrumb.Item onClick={() => history.push('/')}>
 									Home
 								</Breadcrumb.Item>
 								<Breadcrumb.Item active>contact</Breadcrumb.Item>
 							</Breadcrumb>
+
 							<h1 style={{ fontSize: '3em' }}>
 								{contact.firstName} {contact.lastName}
 							</h1>{' '}
@@ -274,17 +344,21 @@ function Contactpage(props) {
 	);
 }
 
+
+
 function App() {
 	return (
 		<div className='App'>
+			<Header />
 			<Switch>
 				<Route exact path='/'>
 					<SearchBar />
 				</Route>
 				<Route path='/contact/:name'>
 					<Contactpage />
-				</Route>
+				</Route>			
 			</Switch>
+			
 		</div>
 	);
 }
